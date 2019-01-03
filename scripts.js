af599e88d746c0be5533c99ef7268d6408d6d002
kipachi <kipachi@mail.ru>
2019-01-04 01:04:04 +0300

    Исправил опечатку

---
+++ scripts.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('.js-smooth-scroll').on('click.smoothscroll', function(e) {
+		e.preventDefault();
+		var $scrollTo = $($.attr(this, 'href'));
+		if ($scrollTo.length) {
+			$('html, body').animate({
+				scrollTop: $scrollTo.offset().top
+			}, 600);
+		}
+	});
+
+	$('.reg-btn.collapsed').click(function() {
+		gtag('event', 'Registration Button Click');
+	});
+});
